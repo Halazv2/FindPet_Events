c8c1bbef66b3a686edb8d761018c2c288f64d802
Halazv2 <halaziani2003@gmail.com>
2024-02-01 15:34:42 +0100

    [frontend] refactor: Header component to use JSX.Element type + fix some esling issues

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -10,7 +10,7 @@ import { navigation } from "@utils/constants";
 import LogoIcon from "@assets/svgs/logo.svg";
 import PawsIcon from "@assets/svgs/paws.svg";
 
-export default function Header() {
+export default function Header(): JSX.Element {
   const navigate = useNavigate();
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
@@ -21,10 +21,12 @@ export default function Header() {
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">FindPet</span>
-            <img className="h-10 w-auto" src={LogoIcon} />
-          </a>
+          <Link to="#" className="-m-1.5 p-1.5">
+            <>
+              <span className="sr-only">FindPet</span>
+              <img className="h-10 w-auto" src={LogoIcon} alt="find-pet-logo" />
+            </>
+          </Link>
         </div>
         <div className="flex lg:hidden">
           <button
@@ -33,7 +35,7 @@ export default function Header() {
             onClick={() => setMobileMenuOpen(true)}
           >
             <span className="sr-only">Open main menu</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            <Bars3Icon className="size-6" aria-hidden="true" />
           </button>
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
@@ -49,7 +51,7 @@ export default function Header() {
         </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <ButtonIcon
-            icon={<img src={PawsIcon} className="w-8" />}
+            icon={<img src={PawsIcon} className="w-8" alt="paws" />}
             text="Log in"
             className="gap-x-1"
             onClick={() => {
@@ -66,23 +68,25 @@ export default function Header() {
         onClose={setMobileMenuOpen}
       >
         <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
-            </a>
+            <Link to="#" className="-m-1.5 p-1.5">
+              <>
+                <span className="sr-only">FindPet</span>
+                <img
+                  className="h-10 w-auto"
+                  src={LogoIcon}
+                  alt="find-pet-logo"
+                />
+              </>
+            </Link>
             <button
               type="button"
               className="text-gray-700 -m-2.5 rounded-md p-2.5"
               onClick={() => setMobileMenuOpen(false)}
             >
               <span className="sr-only">Close menu</span>
-              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+              <XMarkIcon className="size-6" aria-hidden="true" />
             </button>
           </div>
           <div className="mt-6 flow-root">
@@ -99,7 +103,10 @@ export default function Header() {
                 ))}
               </div>
               <div className="py-6">
-                <ButtonIcon icon={<img src={PawsIcon} />} text="Log in" />
+                <ButtonIcon
+                  icon={<img src={PawsIcon} alt="paws" />}
+                  text="Log in"
+                />
               </div>
             </div>
           </div>
