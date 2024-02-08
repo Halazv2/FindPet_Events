27809eaf97b0ca4ad8642df7aac6c962cd3e630d
Halazv2 <halaziani2003@gmail.com>
2024-02-08 11:43:49 +0100

    [frontend] Refactor Home component to include new components

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 
-import { HomeHero } from "@components/hero";
+import { About } from "@components/home/About";
+import { HomeHero } from "@components/home/Hero";
+import { Projects } from "@components/home/Projects";
+import { Sponsors } from "@components/home/Sponsors";
 
 const Home: React.FC = () => {
   return (
     <div>
       <HomeHero />
+      <div className="flex w-full flex-col items-center justify-center bg-white">
+        <Sponsors />
+        <About />
+      </div>
+      <div>
+        <Projects />
+      </div>
     </div>
   );
 };
