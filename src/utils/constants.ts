8f48870a9f8286572b23a9958ab405463836ea57
Halazv2 <halaziani2003@gmail.com>
2024-02-08 11:43:34 +0100

    [Frontend] Add new icons and update constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,11 @@
+import {
+  Image1,
+  Image2,
+  Image3,
+  Image4,
+  Image5
+} from "@src/assets/Associations";
+
 export const navigation = [
   { name: "Home", href: "#" },
   { name: "Events", href: "#" },
@@ -5,4 +13,12 @@ export const navigation = [
   { name: "Donate", href: "#" },
   { name: "Volunteer", href: "#" },
   { name: "Contact", href: "#" }
+];
+
+export const sponsors = [
+  { name: Image1, href: "#", alt: "Sponsor 1" },
+  { name: Image2, href: "#", alt: "Sponsor 2" },
+  { name: Image3, href: "#", alt: "Sponsor 3" },
+  { name: Image4, href: "#", alt: "Sponsor 4" },
+  { name: Image5, href: "#", alt: "Sponsor 5" }
 ];
