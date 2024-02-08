cf87824957ab504e8dccc6f62d6902b77c7c555e
Halazv2 <halaziani2003@gmail.com>
2024-02-08 11:43:16 +0100

    [frontend] Fix import order in __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,6 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 
 import Layout from "@layouts/index";
 
