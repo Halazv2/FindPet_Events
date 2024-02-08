4fa14af7d369a18b3bd1e591e8a2ca4bda8e176e
Halazv2 <halaziani2003@gmail.com>
2024-02-08 11:43:04 +0100

    [Frontend] Replace AssociationsCard with Sponsors in events route

--- src/routes/events/$eventsId.lazy.tsx
+++ src/routes/events/$eventsId.lazy.tsx
@@ -1,7 +1,7 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
 
-import { AssociationsCard } from "@components/cards/AssociationsCard";
+import { Sponsors } from "@components/home/Sponsors";
 
 export const Route = createLazyFileRoute("/events/$eventsId")({
-  component: AssociationsCard
+  component: Sponsors
 });
