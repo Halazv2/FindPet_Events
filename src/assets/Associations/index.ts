bebaf60b8affcb316c13b9906ac0497b681dda9b
Halazv2 <halaziani2003@gmail.com>
2024-02-08 11:40:49 +0100

    [Frontend] Add: new images and SVG files

---
+++ src/assets/Associations/index.ts
@@ -0,0 +1,7 @@
+import Image1 from "./Image1.png";
+import Image2 from "./Image2.png";
+import Image3 from "./Image3.png";
+import Image4 from "./Image4.png";
+import Image5 from "./Image5.png";
+
+export { Image1, Image2, Image3, Image4, Image5 };
